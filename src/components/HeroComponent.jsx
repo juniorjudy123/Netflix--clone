7a64b815034b2210f0aeb1c0d36318d510a676f8
benoy_Judy <benoyjudy@gmail.com>
2024-11-12 15:30:15 +0530

    Hero component creation

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -7,7 +7,20 @@ const HeroComponent = () => {
 	const playingTrailer = movies ? movies[0] : null
 	const { original_title, overview } = playingTrailer
 
-	return <div></div>
+	return (
+		<div className="pt-40 px-12">
+			<h1 className=" text-4xl font-bold my-2">{original_title}</h1>
+			<p className=" text-lg w-2/4">{overview}</p>
+			<div className="mt-4">
+				<button className="bg-white text-black p-2 px-16 border border-gray-400 mr-3 cursor-pointer">
+					▶️play
+				</button>
+				<button className=" bg-gray-400 text-black p-2 px-16 border border-gray-400 cursor-pointer ">
+					ℹ️ More info
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default HeroComponent
