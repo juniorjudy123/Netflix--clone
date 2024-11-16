d72ed043fbbcb204f4075ae15de195cdbfb07459
benoy_Judy <benoyjudy@gmail.com>
2024-11-16 12:46:39 +0530

    setting up the trailer and overview

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -4,18 +4,21 @@ import { useSelector } from "react-redux"
 const HeroComponent = () => {
 	const movies = useSelector((store) => store.movies?.nowPlayingMovies)
 
-	const playingTrailer = movies ? movies[0] : null
+	if (movies === null) return
+
+	const playingTrailer = movies[2]
+
 	const { original_title, overview } = playingTrailer
 
 	return (
 		<div className="pt-40 px-12">
 			<h1 className=" text-4xl font-bold my-2">{original_title}</h1>
-			<p className=" text-lg w-2/4">{overview}</p>
+			<p className=" text-lg w-1/4">{overview}</p>
 			<div className="mt-4">
-				<button className="bg-white text-black p-2 px-16 border border-gray-400 mr-3 cursor-pointer">
-					▶️play
+				<button className="bg-white text-black p-2 px-16 border border-gray-400 mr-3 cursor-pointer text-md rounded-sm font-semibold bg-opacity-40  hover:bg-white hover:text-black hover:scale-105 active:scale-95 active:bg-[#0048d5] transition-all duration-200  ">
+					▶️Play
 				</button>
-				<button className=" bg-gray-400 text-black p-2 px-16 border border-gray-400 cursor-pointer ">
+				<button className=" bg-gray-400 text-black p-2 px-16 border border-gray-400 cursor-pointer text-md  font-semibold rounded-sm  bg-opacity-40  hover:bg-white hover:text-black hover:scale-105 active:scale-95 active:bg-[#0048d5] transition-all duration-200 ">
 					ℹ️ More info
 				</button>
 			</div>
---
+++ src/components/TrailerComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TrailerComponent = () => {
+	return <div>TrailerComponent</div>
+}
+
+export default TrailerComponent
